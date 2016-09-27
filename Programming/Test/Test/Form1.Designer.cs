eb4b9eee9fe395c86b3890e601485640af280224
PublicString <BG357@student.anglia.ac.uk>
2016-09-27 19:01:32 +0100

    Ignore this

---
+++ Programming/Test/Test/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Test
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.passLabel = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.signupBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userBox
+            // 
+            this.userBox.Font = new System.Drawing.Font("DengXian Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userBox.Location = new System.Drawing.Point(173, 43);
+            this.userBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(201, 23);
+            this.userBox.TabIndex = 0;
+            // 
+            // passBox
+            // 
+            this.passBox.Font = new System.Drawing.Font("DengXian Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passBox.Location = new System.Drawing.Point(173, 74);
+            this.passBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.passBox.Name = "passBox";
+            this.passBox.PasswordChar = '*';
+            this.passBox.Size = new System.Drawing.Size(201, 23);
+            this.passBox.TabIndex = 1;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Font = new System.Drawing.Font("DengXian Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLabel.Location = new System.Drawing.Point(66, 46);
+            this.userLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(74, 15);
+            this.userLabel.TabIndex = 2;
+            this.userLabel.Text = "Username:";
+            // 
+            // passLabel
+            // 
+            this.passLabel.AutoSize = true;
+            this.passLabel.Font = new System.Drawing.Font("DengXian Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passLabel.Location = new System.Drawing.Point(74, 78);
+            this.passLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.passLabel.Name = "passLabel";
+            this.passLabel.Size = new System.Drawing.Size(68, 15);
+            this.passLabel.TabIndex = 3;
+            this.passLabel.Text = "Password:";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Font = new System.Drawing.Font("DengXian Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.Location = new System.Drawing.Point(173, 104);
+            this.loginBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(96, 26);
+            this.loginBtn.TabIndex = 4;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // signupBtn
+            // 
+            this.signupBtn.Font = new System.Drawing.Font("DengXian Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signupBtn.Location = new System.Drawing.Point(279, 104);
+            this.signupBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.signupBtn.Name = "signupBtn";
+            this.signupBtn.Size = new System.Drawing.Size(96, 26);
+            this.signupBtn.TabIndex = 4;
+            this.signupBtn.Text = "Signup";
+            this.signupBtn.UseVisualStyleBackColor = true;
+            this.signupBtn.Click += new System.EventHandler(this.signupBtn_Click);
+            // 
+            // loginForm
+            // 
+            this.AcceptButton = this.loginBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 160);
+            this.Controls.Add(this.signupBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passLabel);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.userBox);
+            this.Font = new System.Drawing.Font("DengXian Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "loginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label passLabel;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button signupBtn;
+    }
+}
+
---
+++ Programming/Test/Test/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class loginForm : Form
+    {
+        private string userDataLoc = "userData.txt";
+
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            if ((!string.IsNullOrEmpty(userBox.Text)) && (!string.IsNullOrEmpty(passBox.Text)))
+            {
+                if (File.ReadAllText(userDataLoc).Contains(Environment.NewLine + userBox.Text + ":"))
+                {
+                    if (File.ReadAllText(userDataLoc).Contains(Environment.NewLine + userBox.Text + ":" + passBox.Text))
+                    {
+                        MessageBox.Show("Logged In");
+                        Application.Exit();
+                    }
+                    else
+                        MessageBox.Show("Password is incorrect, please try again");
+                }
+                else
+                    MessageBox.Show("Username doesn't exist, try signing up.");
+            }
+            else
+                MessageBox.Show("Please enter your login information.");
+        }
+
+        private void signupBtn_Click(object sender, EventArgs e)
+        {
+            if ((!string.IsNullOrEmpty(userBox.Text)) && (!string.IsNullOrEmpty(passBox.Text)))
+            {
+                if (!File.Exists(userDataLoc))
+                {
+                    File.Create(userDataLoc);
+                }
+                else
+                {
+                    if (File.ReadAllText(userDataLoc).Contains(Environment.NewLine + userBox.Text + ":"))
+                    {
+                        MessageBox.Show("Username already exists, please try another.");
+                    }
+                    else
+                        WriteData(userBox.Text, passBox.Text);
+                }
+            }
+        }
+
+        private void WriteData(string username, string password)
+        {
+            File.AppendAllText(userDataLoc, Environment.NewLine + username + ":" + password);
+        }
+    }
+}
