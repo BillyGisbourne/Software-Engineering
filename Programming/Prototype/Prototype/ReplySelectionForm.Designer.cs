1581e1e8aa07cf16ee5fc6c6f68424725491e016
PublicString <BG357@student.anglia.ac.uk>
2016-09-29 19:18:03 +0100

    Added Prototype V1.0.0

---
+++ Programming/Prototype/Prototype/ReplySelectionForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Prototype
+{
+    partial class ReplySelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReplySelectionForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.DatabaseGrabBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AcceptRadio = new System.Windows.Forms.RadioButton();
+            this.DeclineRadio = new System.Windows.Forms.RadioButton();
+            this.ShowCLBtn = new System.Windows.Forms.Button();
+            this.HideCLBtn = new System.Windows.Forms.Button();
+            this.CoverLetterBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // DatabaseGrabBtn
+            // 
+            resources.ApplyResources(this.DatabaseGrabBtn, "DatabaseGrabBtn");
+            this.DatabaseGrabBtn.Name = "DatabaseGrabBtn";
+            this.DatabaseGrabBtn.UseVisualStyleBackColor = true;
+            this.DatabaseGrabBtn.Click += new System.EventHandler(this.DatabaseGrabBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // AcceptRadio
+            // 
+            resources.ApplyResources(this.AcceptRadio, "AcceptRadio");
+            this.AcceptRadio.Checked = true;
+            this.AcceptRadio.Name = "AcceptRadio";
+            this.AcceptRadio.TabStop = true;
+            this.AcceptRadio.UseVisualStyleBackColor = true;
+            // 
+            // DeclineRadio
+            // 
+            resources.ApplyResources(this.DeclineRadio, "DeclineRadio");
+            this.DeclineRadio.Name = "DeclineRadio";
+            this.DeclineRadio.UseVisualStyleBackColor = true;
+            // 
+            // ShowCLBtn
+            // 
+            resources.ApplyResources(this.ShowCLBtn, "ShowCLBtn");
+            this.ShowCLBtn.Name = "ShowCLBtn";
+            this.ShowCLBtn.UseVisualStyleBackColor = true;
+            this.ShowCLBtn.Click += new System.EventHandler(this.ShowCLBtn_Click);
+            // 
+            // HideCLBtn
+            // 
+            resources.ApplyResources(this.HideCLBtn, "HideCLBtn");
+            this.HideCLBtn.Name = "HideCLBtn";
+            this.HideCLBtn.UseVisualStyleBackColor = true;
+            this.HideCLBtn.Click += new System.EventHandler(this.HideCLBtn_Click);
+            // 
+            // CoverLetterBox
+            // 
+            resources.ApplyResources(this.CoverLetterBox, "CoverLetterBox");
+            this.CoverLetterBox.Name = "CoverLetterBox";
+            this.CoverLetterBox.ReadOnly = true;
+            // 
+            // ReplySelectionForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.DeclineRadio);
+            this.Controls.Add(this.AcceptRadio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DatabaseGrabBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CoverLetterBox);
+            this.Controls.Add(this.ShowCLBtn);
+            this.Controls.Add(this.HideCLBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ReplySelectionForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Load += new System.EventHandler(this.ReplySelectionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DatabaseGrabBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton AcceptRadio;
+        private System.Windows.Forms.RadioButton DeclineRadio;
+        private System.Windows.Forms.Button ShowCLBtn;
+        private System.Windows.Forms.Button HideCLBtn;
+        private System.Windows.Forms.TextBox CoverLetterBox;
+    }
+}
+
---
+++ Programming/Prototype/Prototype/ReplySelectionForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype
+{
+
+    public partial class ReplySelectionForm : Form
+    {
+        private int counter = 0;
+        private string PDFLoc = "pdf.txt";
+        private string reply;
+
+        public ReplySelectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ReplySelectionForm_Load(object sender, EventArgs e)
+        {
+            NextApplicant();
+        }
+
+        private void DatabaseGrabBtn_Click(object sender, EventArgs e)
+        {
+            ReplyToApplicant();
+        }
+
+        private void ReplyToApplicant()
+        {
+            if (AcceptRadio.Checked == true)
+            {
+                reply = "Dear " + label2.Text + ","
+                        + Environment.NewLine
+                        + Environment.NewLine +
+                        "We are pleased to accept you as blah blah blah..."
+                        + Environment.NewLine
+                        + Environment.NewLine +
+                        "Thanks,"
+                        +Environment.NewLine +
+                        "HappyTech";
+            }
+            if (DeclineRadio.Checked == true)
+            {
+                reply = "Dear " + label2.Text + ","
+                        + Environment.NewLine
+                        + Environment.NewLine +
+                        "We are sad to decline you as blah blah blah..."
+                        + Environment.NewLine
+                        + Environment.NewLine +
+                        "Sorry,"
+                        + Environment.NewLine +
+                        "HappyTech";
+            }
+
+            WriteToFile(PDFLoc, reply);
+            NextApplicant();
+        }
+
+        private void WriteToFile(string location, string text)
+        {
+            string spacer = Environment.NewLine + 
+                            "--------------------------------------------"
+                            + Environment.NewLine;
+
+            File.AppendAllText(location, text);
+            File.AppendAllText(location, spacer);
+        }
+
+        private void NextApplicant()
+        {
+            counter++;
+            label2.Text = "Test" + counter;
+            label4.Text = "Test@hotmail.com" + counter;
+            CoverLetterBox.Text = "Cover letter fill = " + label2.Text + label4.Text;
+        }
+
+        private void ShowCoveringLetter()
+        {
+            AcceptRadio.Location = new Point(170, 580);
+            DeclineRadio.Location = new Point(285, 580);
+            DatabaseGrabBtn.Location = new Point(170, 603);
+            ReplySelectionForm.ActiveForm.Size = new Size(502, 678);
+
+            CoverLetterBox.Show();
+            ShowCLBtn.Hide();
+            HideCLBtn.Show();
+        }
+
+        private void HideCoveringLetter()
+        {
+            AcceptRadio.Location = new Point(170, 115);
+            DeclineRadio.Location = new Point(285, 115);
+            DatabaseGrabBtn.Location = new Point(170, 138);
+            ReplySelectionForm.ActiveForm.Size = new Size(502, 218);
+
+            CoverLetterBox.Hide();
+            ShowCLBtn.Show();
+            HideCLBtn.Hide();
+        }
+
+        private void ShowCLBtn_Click(object sender, EventArgs e)
+        {
+            ShowCoveringLetter();
+        }
+
+        private void HideCLBtn_Click(object sender, EventArgs e)
+        {
+            HideCoveringLetter();
+        }
+    }
+}
